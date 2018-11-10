b400e53f7e75bd4877f955d56964213ffd79696b
IMO\pgu <pgu@imo-online.com>
2018-11-10 17:45:07 -0600

    Initial Commit

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return 'Hello World! this is a test website'
 
 
+@app.route('/api/testing', methods=['GET'])
+def return_testing_message():
+    return "200 OK!"
+
+
 if __name__ == '__main__':
     app.run()
