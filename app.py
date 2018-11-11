8f365ebf5658f686304b710f341dda2c9c9268df
IMO\pgu <pgu@imo-online.com>
2018-11-10 22:18:16 -0600

    finished course GPA info endpoint

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import *
 import csv
+import pandas as pd
 import get_gpa_data
+
+
 app = Flask(__name__)
 
-fd = open("dataSource/uiuc-gpa-dataset-withGPA.csv", 'r')
-csvReader = csv.reader(fd, delimiter=',')
+# fd = open("dataSource/uiuc-gpa-dataset-withGPA.csv", 'r')
+# csvReader = csv.reader(fd, delimiter=',')
+# headers = next(csvReader, None)
+df = pd.read_csv("dataSource/uiuc-gpa-dataset-withGPA.csv")
+df['Term'] = df['Term'].astype('str')
+df['YearTerm'] = df['YearTerm'].astype('str')
+df['Subject'] = df['Subject'].astype('str')
+df['Course Title'] = df['Course Title'].astype('str')
 
 
 @app.route('/')
@@ -12,9 +21,10 @@ def hello_world():
     return 'Hello World! this is a test website'
 
 
-@app.route('/api/course', methods=['GET'])
-def get_course_gpa():
-    return get_gpa_data.get_frist_row(csvReader)
+# api/course/:subject/:number
+@app.route('/api/course/<string:Subject>/<int:Number>', methods=['GET'])
+def get_course_gpa(Subject, Number):
+    return jsonify(get_gpa_data.get_course_info(Subject, Number, df))
 
 
 @app.route('/api/testing', methods=['GET'])
--- get_gpa_data.py
+++ get_gpa_data.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from flask import *
 
 
@@ -8,3 +9,19 @@ def get_frist_row(csvReader):
         retval = row
         break
     return jsonify(retval)
+
+
+# schema: {subject, number, course name, average gpa, [{instructor A: GPA}, {instructor B: GPA}....}]}
+def get_course_info(subject, number, df):
+    is_target = (df['Subject'] == subject) & (df['Number'] == number)
+    all_offerings = df[is_target]
+    average_gpa = round(all_offerings['Average Grade'].mean(), 2)
+    grouped = all_offerings['Average Grade'].groupby(df['Primary Instructor'])
+    instructor_course_gpa = grouped.mean().round(2)
+    retval = {}
+    retval['Subject'] = subject
+    retval['Number'] = number
+    retval['Course Title'] = all_offerings['Course Title'].values.tolist()[0]
+    retval['Average GPA'] = average_gpa
+    retval['Instructors'] = instructor_course_gpa.to_dict()
+    return retval
